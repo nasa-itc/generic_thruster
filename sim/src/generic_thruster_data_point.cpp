bb30f29ad5c9ebe8a8c597f96745cb85367a804b
Mark Suder <mark.d.suder@nasa.gov>
2024-12-04 12:24:31 -0500

    https://github.com/nasa/nos3/issues/351 - Fix initialization and usage of _not_parsed flag.

--- sim/src/generic_thruster_data_point.cpp
+++ sim/src/generic_thruster_data_point.cpp
@@ -5,62 +5,57 @@ namespace Nos3
 {
     extern ItcLogger::Logger *sim_logger;
 
-    Generic_thrusterDataPoint::Generic_thrusterDataPoint(double count)
+    Generic_thrusterDataPoint::Generic_thrusterDataPoint(double count) : _not_parsed(false)
     {
         sim_logger->trace("Generic_thrusterDataPoint::Generic_thrusterDataPoint:  Defined Constructor executed");
 
         /* Do calculations based on provided data - also preparing like ADC data to checkout is obvious */
-        _42_parsing = false;
         _generic_thruster_data_is_valid = true;
         _generic_thruster_data[0] = (((count * 1) / 32767.0) - 32768.0);
         _generic_thruster_data[1] = (((count * 2) / 32767.0) - 32768.0);
         _generic_thruster_data[2] = (((count * 3) / 32767.0) - 32768.0);
     }
 
-    Generic_thrusterDataPoint::Generic_thrusterDataPoint(int16_t spacecraft, const boost::shared_ptr<Sim42DataPoint> dp) : _dp(*dp), _sc(spacecraft)
+    Generic_thrusterDataPoint::Generic_thrusterDataPoint(int16_t spacecraft, const boost::shared_ptr<Sim42DataPoint> dp) : _dp(*dp), _sc(spacecraft), _not_parsed(true)
     {
         sim_logger->trace("Generic_thrusterDataPoint::Generic_thrusterDataPoint:  42 Constructor executed");
 
         /* Initialize data */
-        _42_parsing = true;
         _generic_thruster_data_is_valid = false;
         _generic_thruster_data[0] = _generic_thruster_data[1] = _generic_thruster_data[2] = 0.0;
     }
     
     void Generic_thrusterDataPoint::do_parsing(void) const
     {
-        if (_42_parsing)
-        {
-            try {
-                /*
-                ** Declare 42 telemetry string prefix
-                ** 42 variables defined in `42/Include/42types.h`
-                ** 42 data stream defined in `42/Source/IPC/SimWriteToSocket.c`
-                */
-                std::string key;
-                key.append("SC[").append(std::to_string(_sc)).append("].svb"); // SC[N].svb
+        try {
+            /*
+            ** Declare 42 telemetry string prefix
+            ** 42 variables defined in `42/Include/42types.h`
+            ** 42 data stream defined in `42/Source/IPC/SimWriteToSocket.c`
+            */
+            std::string key;
+            key.append("SC[").append(std::to_string(_sc)).append("].svb"); // SC[N].svb
 
-                /* Parse 42 telemetry */
-                std::string values = _dp.get_value_for_key(key);
+            /* Parse 42 telemetry */
+            std::string values = _dp.get_value_for_key(key);
 
-                std::vector<double> data;
-                data.reserve(3);
-                parse_double_vector(values, data);
+            std::vector<double> data;
+            data.reserve(3);
+            parse_double_vector(values, data);
 
-                _generic_thruster_data[0] = data[0];
-                _generic_thruster_data[1] = data[1];
-                _generic_thruster_data[2] = data[2];
+            _generic_thruster_data[0] = data[0];
+            _generic_thruster_data[1] = data[1];
+            _generic_thruster_data[2] = data[2];
 
-                /* Mark data as valid */
-                _generic_thruster_data_is_valid = true;
+            /* Mark data as valid */
+            _generic_thruster_data_is_valid = true;
 
-                _not_parsed = false;
+            _not_parsed = false;
 
-                /* Debug print */
-                sim_logger->trace("Generic_thrusterDataPoint::Generic_thrusterDataPoint:  Parsed svb = %f %f %f", _generic_thruster_data[0], _generic_thruster_data[1], _generic_thruster_data[2]);
-            } catch (const std::exception &e) {
-                sim_logger->error("Generic_thrusterDataPoint::Generic_thrusterDataPoint:  Error parsing svb.  Error=%s", e.what());
-            }
+            /* Debug print */
+            sim_logger->trace("Generic_thrusterDataPoint::Generic_thrusterDataPoint:  Parsed svb = %f %f %f", _generic_thruster_data[0], _generic_thruster_data[1], _generic_thruster_data[2]);
+        } catch (const std::exception &e) {
+            sim_logger->error("Generic_thrusterDataPoint::Generic_thrusterDataPoint:  Error parsing svb.  Error=%s", e.what());
         }
     }
 
