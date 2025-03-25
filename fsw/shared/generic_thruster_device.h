437eaf2d64a5d19be4d34884cbd133f02cc149bc
rjbrown6 <Robert.J.Brown@nasa.gov>
2025-03-25 12:11:40 -0400

    Revert "style: auto-format via clang-format"
    This reverts commit 9f3c7834f017b1f23988d2c13be984d4ab9eae69.

--- fsw/shared/generic_thruster_device.h
+++ fsw/shared/generic_thruster_device.h
@@ -12,29 +12,29 @@
 ** Required header files.
 */
 #include "device_cfg.h"
-#include "generic_thruster_platform_cfg.h"
 #include "hwlib.h"
+#include "generic_thruster_platform_cfg.h"
+
 
 /*
 ** Type definitions
 ** TODO: Make specific to your application
 */
-#define GENERIC_THRUSTER_DEVICE_HDR 0xDEAD
-#define GENERIC_THRUSTER_DEVICE_HDR_0 0xDE
-#define GENERIC_THRUSTER_DEVICE_HDR_1 0xAD
+#define GENERIC_THRUSTER_DEVICE_HDR              0xDEAD
+#define GENERIC_THRUSTER_DEVICE_HDR_0            0xDE
+#define GENERIC_THRUSTER_DEVICE_HDR_1            0xAD
 
-#define GENERIC_THRUSTER_DEVICE_TRAILER 0xBEEF
-#define GENERIC_THRUSTER_DEVICE_TRAILER_0 0xBE
-#define GENERIC_THRUSTER_DEVICE_TRAILER_1 0xEF
+#define GENERIC_THRUSTER_DEVICE_TRAILER          0xBEEF
+#define GENERIC_THRUSTER_DEVICE_TRAILER_0        0xBE
+#define GENERIC_THRUSTER_DEVICE_TRAILER_1        0xEF
 
-#define GENERIC_THRUSTER_DEVICE_HDR_TRL_LEN 4
-#define GENERIC_THRUSTER_DEVICE_CMD_SIZE 6
+#define GENERIC_THRUSTER_DEVICE_HDR_TRL_LEN      4
+#define GENERIC_THRUSTER_DEVICE_CMD_SIZE         6
 
 /*
 ** Prototypes
 */
-int32_t GENERIC_THRUSTER_SetPercentage(uart_info_t *device,
-                                       uint8_t thruster_number,
-                                       uint8_t percentage, uint8_t data_length);
+int32_t GENERIC_THRUSTER_SetPercentage(uart_info_t *device, uint8_t thruster_number, uint8_t percentage, uint8_t data_length);
+
 
 #endif /* _GENERIC_THRUSTER_DEVICE_H_ */
